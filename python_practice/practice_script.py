dba2f359234d328d078fadbd349c62cd9a7803d8
Anastasia Spiridonova <aspirid000@citymail.cuny.edu>
2017-05-31 15:40:01 -0400

    Added sample class to play with called Algebra in practice_script.py

--- python_practice/practice_script.py
+++ python_practice/practice_script.py
@@ -3,3 +3,31 @@ This script contains an example class that we will write. The class will be call
 
 The script will end with a sample way to call and use the class
 '''
+sample_int =5 
+
+class Algebra:
+   def __init__( self ,x, y ) :
+       #We defining the arguments as attributes of
+       #the object itself
+       self.x = x
+       self.y = y
+
+   def add( self ) :
+       return self.x + self.y
+
+   #Exercise: Define a subtract, divide, and multiply method.
+
+   def subtract( self) :
+       return self.x - self.y
+
+   def multiply( self ) :
+       return self.x * self.y
+   
+   def divide( self ) :
+       return self.x / self.y
+
+   def complicated_algebra ( self ) :
+      return self.multiply() + self.subtract ()
+   
+   def modify_x( self ) :
+      self.x = self.x * 2
